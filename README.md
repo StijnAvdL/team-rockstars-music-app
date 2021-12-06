1ea5d0569684bbfc0f217633e0328a0d67f04a1e
stijnavdl <stijn@orikami.nl>
2021-12-06 19:10:51 +0100

    first commit

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# Janssen demo app
+# Team Rockstars Music app
 
-The demo app for the Janssen project. With two biomarkers:
-
-- Walking speed test
-- Cognition (SDMT) test
+The Team Eockstars Music app project.
 
 ### Install
 
@@ -13,9 +10,7 @@ Requirements:
 - Cordova
 
 ```
-git clone https://github.com/orikami-nl/janssen-demo-app.git
-npm uninstall @orikami-nl/orikami-components --save
-npm install @orikami-nl/orikami-components --registry https://npm.pkg.github.com
+npm install
 ```
 
 ## Development
@@ -27,12 +22,6 @@ npm run start
 ```
 
 **Update code cordova**
-Update the following files with the correct **CodePushDeploymentKey**. You can find the key in the dashboard of AppCenter under: Distribute -> CodePush -> right corner settings icon. Do this for both iOS and Android:
-
-- cordova/config.xml
-
 ```
 npm run cordova
-cd cordova
-npm run release
 ```
--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -1,96 +1,28 @@
 import PropTypes from "prop-types";
 import { useObserver } from "mobx-react";
 
-import { AppBar } from "@orikami-nl/orikami-components";
-import Sdmt from "@orikami-nl/sdmt-frontend";
-import Tug from "@orikami-nl/tug-frontend";
-import Info from "/src/components/Info";
-
-import HelpIcon from '@material-ui/icons/HelpOutline';
-
-import Home from "/src/viewmodels/Home";
-import Menu from "/src/components/Menu";
-import MenuModel from "/src/models/Menu";
-
-import Timeline from "/src/models/Timeline";
-
 const propTypesView = {
   page: PropTypes.string,
   go: PropTypes.func,
   params: PropTypes.object
 };
 
-const timeline = new Timeline();
-
 function Router(props) {
   const { page, go } = props;
   var content = null;
-  var appBar = <AppBar
-    leftType="menu"
-    title="MM Fit"
-    leftAction={MenuModel.toggle}
-    rightAction={() => { go("/info") }}
-    rightIcon={<HelpIcon />} />;
+
 
   switch (page) {
     case "/":
-      content = (
-        <Home go={go} timeline={timeline} />
-      );
+      content = <p>Home</p>;
       break;
-    case "/info":
-      appBar = <AppBar
-        leftType="menu"
-        title="MM Fit"
-        leftAction={() => go("/")}
-        leftType={"back"} />;
-      content = <Info />;
-      break;
-    case "/cognition":
-      content = (
-        <Sdmt
-          fullServerUrl="https://alpha.orikami-api.nl/v1/janssen-demo"
-          language="nl"
-          exitTest={() => go("/")}
-          finishTest={(data) => {
-            timeline.all.unshift({
-              type: "sdmt",
-              timestamp: new Date(),
-              value: `${data.value.correct}/${data.value.nb}`
-            });
-          }}
-          restart={false}
-          practiceBeforeTest={true}
-          disableRipple={true}
-          overlay={true}
-          sensorData={true}
-          s3Bucket="orikami-cordova-plugin"
-          s3Prefix="dev/uploader/"
-        />
-      );
-      appBar = null;
-      break;
-    case "/tug":
-      content = (
-        <Tug
-          language="nl"
-          exitTest={() => go("/")}
-          finishTest={(data) => {
-            timeline.all.unshift({
-              type: "tug",
-              timestamp: new Date(),
-              value: `${Math.round(data.value * 10) / 10} sec`
-            });
-          }}
-          serverUrl="https://alpha.orikami-api.nl/v1/janssen-demo" />
-      );
-      appBar = null;
+    case "/error":
+      content = <p>Error</p>;
       break;
   }
 
   return useObserver(() => (
     <div>
-      {appBar}
       {content}
       <Menu go={go} />
     </div>
